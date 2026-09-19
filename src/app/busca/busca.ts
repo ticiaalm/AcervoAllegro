bf4365b1d5f5c837deec116bb9b9eff856c66dfa
Letícia Almeida <leticiaalmeidadev@gmail.com>
2026-09-18 22:06:03 -0300

    feat: adição de pastas da página

---
+++ src/app/busca/busca.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-busca',
+  styleUrl: './busca.css',
+  templateUrl: './busca.html',
+})
+export class Busca {}
